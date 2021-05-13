667e9f649ca3f6cd1fa6db73713d1d3584ffb953
letz0703 <letz0703@gmail.com>
2021-05-13 13:44:21 +0900

    Finish design of Game Activity

--- app/src/main/java/com/letz/icanmart/GuessingGameActivity.java
+++ app/src/main/java/com/letz/icanmart/GuessingGameActivity.java
@@ -3,12 +3,30 @@ package com.letz.icanmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GuessingGameActivity extends AppCompatActivity {
+    private TextView TextViewLast, TextViewRight, TextViewHint;
+    private Button ButtonConfirm;
+    private EditText EditTextGuess;
+    boolean twoDigits, threeDigits, fourDigits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guessing_game);
+
+        TextViewLast = findViewById(R.id.textViewLast);
+        TextViewRight = findViewById(R.id.textViewRight);
+        TextViewHint = findViewById(R.id.textViewHint);
+        ButtonConfirm = findViewById(R.id.buttonConfirm);
+        EditTextGuess = findViewById(R.id.editTextTextGuess);
+
+        twoDigits = getIntent().getBooleanExtra("two", false);
+        threeDigits = getIntent().getBooleanExtra("three", false);
+        fourDigits = getIntent().getBooleanExtra("four", false);
+        
     }
 }
